f8d3d3d05f52010fa9e182971f02a4e4aa29edf9
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-08-28 12:15:55 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,7 +2,6 @@
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
-from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
